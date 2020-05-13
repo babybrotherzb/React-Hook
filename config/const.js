1426c5c628eccb3addc410fea31d9f5050a689a1
张宝 <zhangbao@mianchao-inc.com>
2020-05-13 13:47:11 +0800

    webpack从node_modules剥离

---
+++ config/const.js
@@ -0,0 +1,43 @@
+const projectName = "react-hook";
+module.exports = {
+  name: "react-hook",
+  app_name: "react-hook",
+  projectName: projectName,
+  is_socket: true, // 是否启用scoket
+  server: {
+    port: 3250
+  },
+  //   hotServer: {
+  //     port: 8999
+  //   },
+  socket: {
+    port: 3025
+  },
+  proxy: {
+    port: 9250
+  },
+  env: [
+    {
+      name: "release",
+      title: "测试环境"
+    },
+    {
+      name: "production",
+      title: "正式环境"
+    }
+  ],
+  qiniu: {
+    // 七牛上传地址
+    release: "http://up.qiniup.com",
+    production: "https://upload.qbox.me"
+  },
+  domain: {
+    // 服务器地址
+    release: "https://react-hook-q1.com/",
+    production: "https://react-hook.com/"
+  },
+  apiUrl: {
+    global: "/api/v1/global",
+    staticToken: "/api/v1/qiniu/static_token"
+  }
+};
